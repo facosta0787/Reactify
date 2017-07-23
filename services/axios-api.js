056218b805da4f1e0f3619a04cd9595d3e4faeea
Felipe Acosta <facosta0787@gmail.com>
2017-07-23 13:50:39 -0500

    Create instance of Axios with defaults configs

--- services/axios-api.js
+++ services/axios-api.js
@@ -2,14 +2,16 @@ import axios from 'axios'
 import utf8enc from 'crypto-js/enc-utf8'
 import base64 from 'crypto-js/enc-base64'
 
+const HTTP = axios.create({
+  baseURL: `https://api.spotify.com/v1`
+})
+
 class Api{
 
   id = '071685b2e4c94a71881fc852abda49a2'
   secret ='8f1195816a444290b1109097161c79b1'
-  url = 'https://api.spotify.com'
 
   get_token(){
-
     const utf8Array = utf8enc.parse(this.id+':'+this.secret)
     const strBase64 = base64.stringify(utf8Array)
     const res = axios({
@@ -22,20 +24,19 @@ class Api{
       data: 'grant_type=client_credentials'
     })
     return res
-
   }
 
   get_data( query,token ){
-    const res = axios({
-      method: 'get',
+    const res = HTTP.get(`/search/?q=${query}&type=artist,album,track`,{
       headers:{
         'Authorization':'Bearer ' + token
-      },
-      url: `${this.url}/v1/search/?q=${query}&type=artist,album,track`
+      }
+    })
+    .then(resp => {
+      return resp
     })
     return res
   }
-
 
 }
 
